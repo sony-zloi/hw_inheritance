75267b6e53461452aac1db8fca4569236c85c01f
Султангулов Данис Радикович <sultangulovdr-bsh@rosneft.ru>
2021-01-25 11:50:20 +0500

    second commit

--- task_1.py
+++ task_1.py
@@ -7,18 +7,38 @@ MeatGrinder (содержит информацию о мясорубке).Каж
 
 
 class Device:
-    pass
+    """Устройство реализующее функционал автоматизации процессов"""
+    button: bool = False
+
+    @classmethod
+    def on_off(cls):
+        if cls.button:
+            return False
+        return True
 
 
 class CoffeeMachine(Device):
-    pass
+    """Устройство для автоматизации процесса приготовления кофе"""
+
+    def __init__(self, water: int, milk: int, coffee_beans: int, cups: int, money: int):
+        self._water = water
+        self._milk = milk
+        self._coffee_beans = coffee_beans
+        self._cups = cups
+        self._money = money
 
 
 class Blender(Device):
-    pass
+    """Устройство автомазирующее процесс замешивания продуктов"""
+    def __init__(self, speed_count: int, capacity: int):
+        self._speed_count = speed_count
+        self._capacity = capacity
 
 
 class MeatGrinder(Device):
-    pass
+    """Устройство автоматизурующее процесс изготовления мясного фарша"""
+    def __init__(self, power: int, capacity: int ):
+        self._power = power
+        self._capacity = capacity
 
 
